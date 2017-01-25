7d96030bc83dacbadd319c24e6deef7153b980b3
GarwelGarwel <okozlovsky@gmail.com>
2017-01-25 13:51:57 +0300

    Rebalanced health factors Assigned added (-1 HP/day) Living Space factor reduced (from -10 to -7 HP/day) Not Alone factor reduced (from +2 to +1) KSC factor reduced (from +10 to +5)

--- FactorsSettings.cs
+++ FactorsSettings.cs
@@ -14,15 +14,15 @@ namespace KerbalHealth
         public override int SectionOrder { get { return 2; } }
 
         [GameParameters.CustomFloatParameterUI("Assigned Factor", toolTip = "HP change per day when the kerbal is assigned", minValue = -100, maxValue = 100, stepCount = 201)]
-        public float AssignedFactor = 0;
+        public float AssignedFactor = -1;
 
         [GameParameters.CustomFloatParameterUI("Living Space Factor", toolTip = "HP change per day in a crammed vessel", minValue = -100, maxValue = 100, stepCount = 201)]
-        public float LivingSpaceBaseFactor = -10;
+        public float LivingSpaceBaseFactor = -7;
 
         [GameParameters.CustomFloatParameterUI("Not Alone Factor", toolTip = "HP change per day when the kerbal has crewmates", minValue = -100, maxValue = 100, stepCount = 201)]
-        public float NotAloneFactor = 2;
+        public float NotAloneFactor = 1;
 
         [GameParameters.CustomFloatParameterUI("KSC Factor", toolTip = "HP change per day when the kerbal is at KSC (available)", minValue = -100, maxValue = 100, stepCount = 201)]
-        public float KSCFactor = 10;
+        public float KSCFactor = 5;
     }
 }
