58a0b18255fe62cb67a381d049819d779a1b3c65
GarwelGarwel <okozlovsky@gmail.com>
2017-02-11 22:45:08 +0300

    More logging

--- MicrogravityFactor.cs
+++ MicrogravityFactor.cs
@@ -20,6 +20,18 @@ namespace KerbalHealth
                 Core.Log("Microgravity factor always on in Editor.");
                 return BaseChangePerDay;
             }
+            if (pcm == null)
+            {
+                Core.Log("MicrogravityFactor.ChangePerDay: pcm is null!", Core.LogLevel.Error);
+                return 0;
+            }
+            if (Core.KerbalHealthList.Find(pcm) == null)
+            {
+                Core.Log(pcm.name + " not found in KerbalHealthList. The list has " + Core.KerbalHealthList.Count + " records.", Core.LogLevel.Error);
+                return 0;
+            }
+            if (pcm.KerbalRef == null) Core.Log("MicrogravityFactor.ChangePerDay: pcm.KerbalRef is null for " + pcm.name + "!", Core.LogLevel.Important);
+            else if (pcm.KerbalRef.InVessel == null) Core.Log("MicrogravityFactor.ChangePerDay: pcm.KerbalRef.InVessel is null for " + pcm.name + ", he/she is " + (Core.KerbalHealthList.Find(pcm).IsOnEVA ? "" : "NOT") + " on EVA!", Core.KerbalHealthList.Find(pcm).IsOnEVA ? Core.LogLevel.Debug : Core.LogLevel.Error);
             if (!Core.KerbalHealthList.Find(pcm).IsOnEVA && (pcm.KerbalRef.InVessel.situation & (Vessel.Situations.ORBITING | Vessel.Situations.SUB_ORBITAL)) != 0)
             {
                 Core.Log("Microgravity is on due to being in " + pcm.KerbalRef.InVessel.situation);
@@ -32,7 +44,6 @@ namespace KerbalHealth
             }
             Core.Log("Microgravity is off, g = " + pcm.geeForce);
             return 0;
-            //Core.IsInEditor || (pcm.KerbalRef.InVessel.situation & (Vessel.Situations.ORBITING | Vessel.Situations.SUB_ORBITAL)) != 0 || (pcm.geeForce < 0.1)
         }
     }
 }
