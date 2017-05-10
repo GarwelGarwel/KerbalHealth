760430213f162201c12323a8bfc6b77d1a110a7e
GarwelGarwel <okozlovsky@gmail.com>
2017-05-10 10:17:48 +0300

    Changed min/max values for some factors in Settings

--- KerbalHealthFactorsSettings.cs
+++ KerbalHealthFactorsSettings.cs
@@ -20,22 +20,22 @@ namespace KerbalHealth
         [GameParameters.CustomFloatParameterUI("Crowded", toolTip = "HP change per day in a crowded vessel", minValue = -50, maxValue = 0, stepCount = 51)]
         public float CrowdedBaseFactor = -5;
 
-        [GameParameters.CustomFloatParameterUI("Loneliness", toolTip = "HP change per day when the kerbal has no crewmates", minValue = -50, maxValue = 0, stepCount = 51)]
+        [GameParameters.CustomFloatParameterUI("Loneliness", toolTip = "HP change per day when the kerbal has no crewmates", minValue = -20, maxValue = 0, stepCount = 41)]
         public float LonelinessFactor = -1;
 
         [GameParameters.CustomFloatParameterUI("Microgravity", toolTip = "HP change per day when in orbital/suborbital flight or g-force < 0.1", minValue = -20, maxValue = 0, displayFormat = "F1", stepCount = 41)]
         public float MicrogravityFactor = -0.5f;
 
-        [GameParameters.CustomFloatParameterUI("EVA", toolTip = "HP change per day when on EVA", minValue = -100, maxValue = 0, stepCount = 51)]
+        [GameParameters.CustomFloatParameterUI("EVA", toolTip = "HP change per day when on EVA", minValue = -50, maxValue = 0, stepCount = 51)]
         public float EVAFactor = -30;
 
-        [GameParameters.CustomFloatParameterUI("Sickness", toolTip = "Base HP change per day when a kerbal is sick", minValue = -50, maxValue = 0, stepCount = 51)]
+        [GameParameters.CustomFloatParameterUI("Sickness", toolTip = "HP change per day when a kerbal is sick", minValue = -50, maxValue = 0, stepCount = 51)]
         public float SicknessFactor = -5;
 
         [GameParameters.CustomFloatParameterUI("Connected", toolTip = "HP change per day when connected to Kerbin", minValue = 0, maxValue = 20, displayFormat = "F1", stepCount = 41)]
         public float ConnectedFactor = 0.5f;
 
-        [GameParameters.CustomFloatParameterUI("Home", toolTip = "HP change per day when in Kerbin atmosphere", minValue = 0, maxValue = 20, stepCount = 41)]
+        [GameParameters.CustomFloatParameterUI("Home", toolTip = "HP change per day when in Kerbin atmosphere", minValue = 0, maxValue = 20, displayFormat = "F1", stepCount = 41)]
         public float HomeFactor = 5;
 
         [GameParameters.CustomFloatParameterUI("At KSC", toolTip = "HP change per day when the kerbal is at KSC (available)", minValue = 0, maxValue = 50, stepCount = 51)]
