66b9686123079a90b2aaa442c4d61e8854e08e38
GarwelGarwel <okozlovsky@gmail.com>
2018-01-25 08:08:29 +0300

    Hopefully fixed NRE in Connected factor

--- ConnectedFactor.cs
+++ ConnectedFactor.cs
@@ -9,7 +9,7 @@
         public override double ChangePerDay(ProtoCrewMember pcm)
         {
             if (Core.IsInEditor) return IsEnabledInEditor() ? BaseChangePerDay : 0;
-            return (Core.IsKerbalLoaded(pcm) && Core.KerbalVessel(pcm).Connection.IsConnectedHome) ? BaseChangePerDay : 0;
+            return (Core.IsKerbalLoaded(pcm) && (Core.KerbalVessel(pcm).Connection != null) && Core.KerbalVessel(pcm).Connection.IsConnectedHome) ? BaseChangePerDay : 0;
         }
     }
 }
