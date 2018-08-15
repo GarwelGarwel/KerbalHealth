dee695717b91735f2d4a350c478f8ad8fccbd442
GarwelGarwel <okozlovsky@gmail.com>
2018-08-15 09:39:58 +0300

    Improved some wording

--- KerbalHealthQuirkSettings.cs
+++ KerbalHealthQuirkSettings.cs
@@ -12,13 +12,13 @@
         [GameParameters.CustomParameterUI("Quirks Enabled", toolTip = "Quirks can be awarded to kerbals and affect their health stats")]
         public bool QuirksEnabled = true;
 
-        [GameParameters.CustomIntParameterUI("Max Quirks", toolTip = "Maximum number of quirks for a kerbal", minValue = 0, maxValue = 5, displayFormat = "N0", stepSize = 1)]
+        [GameParameters.CustomIntParameterUI("Max Quirks", toolTip = "Maximum number of level-up quirks for a kerbal", minValue = 0, maxValue = 5, displayFormat = "N0", stepSize = 1)]
         public int MaxQuirks = 2;
 
         [GameParameters.CustomFloatParameterUI("Level-Up Quirk Chance", toolTip = "Chance of a kerbal being awarded a quirk when he/she levels up", minValue = 0, maxValue = 1, displayFormat = "N2", asPercentage = true, stepCount = 21)]
         public float QuirkChance = 0.25f;
 
-        [GameParameters.CustomParameterUI("Award on Missions", toolTip = "Level-up quirks can be awarded when the kerbal is assigned, otherwise only at KSC")]
+        [GameParameters.CustomParameterUI("Award during Missions", toolTip = "Level-up quirks can be awarded when the kerbal is assigned, otherwise only at KSC")]
         public bool AwardQuirksOnMissions = false;
 
         [GameParameters.CustomFloatParameterUI("Anomaly Quirk Chance", toolTip = "Chance of a kerbal being awarded a quirk when he/she discovers an anomaly", minValue = 0, maxValue = 1, displayFormat = "N2", asPercentage = true, stepCount = 21)]
