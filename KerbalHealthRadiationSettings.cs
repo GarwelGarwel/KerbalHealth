d75aac7aeec319b165754a69128e1b0c6013d54c
GarwelGarwel <okozlovsky@gmail.com>
2018-08-28 18:25:40 +0300

    Radiation settings changed to more realistic (overall cosmic radiation decreased to 5000 @ 1 AU; in-space low level increased to 30%; stratosphere and troposphere coefficients decreased to 20% and 1%)

--- KerbalHealthRadiationSettings.cs
+++ KerbalHealthRadiationSettings.cs
@@ -52,22 +52,22 @@
         [GameParameters.CustomFloatParameterUI("In Space High Coefficient", toolTip = "How much cosmic radiation reaches vessels in high planetary orbit or on moons", minValue = 0, maxValue = 1, displayFormat = "N2", asPercentage = true, stepCount = 21)]
         public float InSpaceHighCoefficient = 0.50f;
 
-        [GameParameters.CustomFloatParameterUI("In Space Low Coefficient", toolTip = "How much cosmic radiation reaches vessels in low planetary orbits", minValue = 0, maxValue = 0.5f, displayFormat = "N2", asPercentage = true, stepCount = 21)]
-        public float InSpaceLowCoefficient = 0.10f;
+        [GameParameters.CustomFloatParameterUI("In Space Low Coefficient", toolTip = "How much cosmic radiation reaches vessels in low planetary orbits", minValue = 0, maxValue = 1, displayFormat = "N2", asPercentage = true, stepCount = 21)]
+        public float InSpaceLowCoefficient = 0.30f;
 
         [GameParameters.CustomFloatParameterUI("Stratosphere Transparency", toolTip = "How much cosmic radiation reaches outer layers of the atmosphere from space", minValue = 0, maxValue = 1, displayFormat = "N2", asPercentage = true, stepCount = 21)]
-        public float StratoCoefficient = 0.3f;
+        public float StratoCoefficient = 0.2f;
 
         [GameParameters.CustomFloatParameterUI("Troposphere Transparency", toolTip = "How much cosmic radiation reaches the ground and lower layers of the atmosphere from space", minValue = 0, maxValue = 0.05f, displayFormat = "N4", asPercentage = true, stepCount = 51)]
-        public float TroposphereCoefficient = 0.02f;
+        public float TroposphereCoefficient = 0.01f;
 
         [GameParameters.CustomFloatParameterUI("EVA Exposure Multiplier", toolTip = "How much more radiaiton kerbals receive when on EVA", minValue = 0, maxValue = 10, displayFormat = "N0", stepCount = 21)]
         public float EVAExposure = 5;
 
-        [GameParameters.CustomFloatParameterUI("Solar Radiation (Nominal)", toolTip = "Solar radiation in interplanetary space at 1 AU, banana doses/day", minValue = 0, maxValue = 20000, displayFormat = "N0", stepCount = 21)]
-        public float SolarRadiation = 5000;
+        [GameParameters.CustomFloatParameterUI("Solar Radiation (Nominal)", toolTip = "Solar radiation in interplanetary space at 1 AU, banana doses/day", minValue = 0, maxValue = 10000, displayFormat = "N0", stepCount = 21)]
+        public float SolarRadiation = 2000;
 
-        [GameParameters.CustomFloatParameterUI("Galactic Radiation", toolTip = "Galactic cosmic radiation in interplanetary space, banana doses/day", minValue = 0, maxValue = 20000, displayFormat = "N0", stepCount = 21)]
-        public float GalacticRadiation = 5000;
+        [GameParameters.CustomFloatParameterUI("Galactic Radiation", toolTip = "Galactic cosmic radiation in interplanetary space, banana doses/day", minValue = 0, maxValue = 10000, displayFormat = "N0", stepCount = 21)]
+        public float GalacticRadiation = 3000;
     }
 }
