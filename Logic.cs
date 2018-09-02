71eb0643515e226a2b0a4726e545e0d27f06cb33
GarwelGarwel <okozlovsky@gmail.com>
2018-09-02 16:32:47 +0300

    Added Situation, Gender, TraitPresent logics

--- Logic.cs
+++ Logic.cs
@@ -15,10 +15,13 @@ namespace KerbalHealth
         public OperatorType Operator { get; set; } = OperatorType.And;
         public bool Inverse { get; set; } = false;
 
+        public string Situation { get; set; } = null;
         public string InSOI { get; set; } = null;
         public string KerbalStatus { get; set; } = null;
         public double MissionTime { get; set; } = Double.NaN;
+        public string Gender { get; set; } = null;
         public string GenderPresent { get; set; } = null;
+        public string TraitPresent { get; set; } = null;
 
         public List<Logic> Operands { get; set; } = new List<Logic>();
 
@@ -42,8 +45,25 @@ namespace KerbalHealth
                 return res;
             }
             Vessel v = Core.KerbalVessel(pcm);
+            if (Situation != null)
+                if (v != null)
+                {
+                    Core.Log("Checking 'situation = " + Situation + "'. " + v.vesselName + " is " + v.situation);
+                    switch (Situation.ToLower())
+                    {
+                        case "prelaunch": Op(ref res, v.situation == Vessel.Situations.PRELAUNCH); break;
+                        case "landed": Op(ref res, v.situation == Vessel.Situations.LANDED); break;
+                        case "splashed": Op(ref res, v.situation == Vessel.Situations.SPLASHED); break;
+                        case "ground": Op(ref res, (v.situation == Vessel.Situations.LANDED) || (v.situation == Vessel.Situations.SPLASHED)); break;
+                        case "flying": Op(ref res, v.situation == Vessel.Situations.FLYING); break;
+                        case "suborbital": Op(ref res, v.situation == Vessel.Situations.SUB_ORBITAL); break;
+                        case "orbiting": Op(ref res, v.situation == Vessel.Situations.ORBITING); break;
+                        case "escaping": Op(ref res, v.situation == Vessel.Situations.ESCAPING); break;
+                        case "in space": Op(ref res, (v.situation == Vessel.Situations.SUB_ORBITAL) || (v.situation == Vessel.Situations.ORBITING) || (v.situation == Vessel.Situations.ESCAPING)); break;
+                    }
+                }
+                else Op(ref res, false);
             if (InSOI != null)
-            {
                 if (v != null)
                 {
                     Core.Log("Checking 'inSOI = " + InSOI + "' rule. " + pcm.name + " is in " + v.mainBody?.name + "'s SOI.");
@@ -54,10 +74,9 @@ namespace KerbalHealth
                     Core.Log("Checking 'inSOI = " + InSOI + "' rule. " + pcm.name + " is not in a vessel => this logic is false.");
                     Op(ref res, false);
                 }
-            }
             if (KerbalStatus != null)
             {
-                Core.Log("Checking 'kerbalStatus = " + KerbalStatus + "'. " + pcm.name + " is " + pcm.rosterStatus + ".");
+                Core.Log("Checking 'kerbalStatus = " + KerbalStatus + "'. " + pcm.name + " is " + pcm.rosterStatus);
                 Op(ref res, KerbalStatus.Equals(pcm.rosterStatus.ToString(), StringComparison.CurrentCultureIgnoreCase));
             }
             if (!Double.IsNaN(MissionTime))
@@ -65,6 +84,16 @@ namespace KerbalHealth
                 Core.Log("Checking 'missionTime = " + MissionTime + "'. MET is " + v?.missionTime + ".");
                 if (v != null) Op(ref res, v.missionTime >= MissionTime);
                 else Op(ref res, false);
+            }
+            if (Gender != null)
+            {
+                ProtoCrewMember.Gender g = pcm.gender;
+                Core.Log("Gender should be " + Gender + " and " + pcm.name + " is " + g);
+                switch (Gender.ToLower())
+                {
+                    case "female": Op(ref res, g == ProtoCrewMember.Gender.Female); break;
+                    case "male": Op(ref res, g == ProtoCrewMember.Gender.Male); break;
+                }
             }
             if (GenderPresent != null)
             {
@@ -91,6 +120,20 @@ namespace KerbalHealth
                 Core.Log(g + " crewmates " + (found ? "" : "not ") + "found.");
                 Op(ref res, found);
             }
+            if (TraitPresent != null)
+            {
+                Core.Log("Checking condition 'traitPresent = " + TraitPresent + "'.");
+                bool found = false;
+                if (v != null)
+                    foreach (ProtoCrewMember crewmate in v.GetVesselCrew())
+                        if (crewmate.trait.ToLower() == TraitPresent.ToLower())
+                        {
+                            found = true;
+                            break;
+                        }
+                Core.Log(TraitPresent + " crewmates " + (found ? "" : "not ") + "found.");
+                Op(ref res, found);
+            }
             foreach (Logic l in Operands)
                 Op(ref res, l.Test(pcm));
             return res ^ Inverse;
@@ -102,10 +145,13 @@ namespace KerbalHealth
             string indent1 = "";
             for (int i = 0; i < level; i++) indent1 += pad;
             string indent2 = indent1 + pad + " ";
+            if (Situation != null) res += "\nIs " + Situation;
             if (InSOI != null) res += "\n" + indent2 + "Kerbal is in the SOI of " + InSOI;
             if (KerbalStatus != null) res += "\n" + indent2 + "Kerbal is" + KerbalStatus;
             if (!Double.IsNaN(MissionTime)) res += "\n" + indent2 + "Mission lasts at least " + Core.ParseUT(MissionTime, false, 100);
+            if (Gender != null) res += "\n" + indent2 + "Kerbal is " + Gender;
             if (GenderPresent != null) res += "\n" + indent2 + GenderPresent + " gender kerbal(s) present in the vessel";
+            if (TraitPresent != null) res += "\n" + indent2 + TraitPresent + " kerbal(s) present in the vessel";
             foreach (Logic l in Operands) res += "\n" + l.Description(level + 1, pad);
             if (Core.CountChars(res, '\n') >= 2)
                 res = indent1 + " " + (Operator == OperatorType.And ? (Inverse ? "One or more" : "All") : (Inverse ? "Any" : "None")) + " of the following conditions are " + ((Operator == OperatorType.And) && Inverse ? "false" : "true") + ":" + res;
@@ -137,11 +183,14 @@ namespace KerbalHealth
                         break;
                 }
                 Inverse = Core.GetBool(value, "inverse");
+                Situation = Core.GetString(value, "situation");
                 InSOI = Core.GetString(value, "inSOI");
                 if (InSOI?.ToLower() == "home") InSOI = FlightGlobals.GetHomeBodyName();
                 KerbalStatus = Core.GetString(value, "kerbalStatus");
                 MissionTime = Core.GetDouble(value, "missionTime", Double.NaN);
+                Gender = Core.GetString(value, "gender");
                 GenderPresent = Core.GetString(value, "genderPresent");
+                TraitPresent = Core.GetString(value, "traitPresent");
                 foreach (ConfigNode node in value.GetNodes("LOGIC"))
                     Operands.Add(new Logic(node));
             }
