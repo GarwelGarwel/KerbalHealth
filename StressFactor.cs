2104c188a78e0018f55e6a57c3e4fcf749842a16
Garwel <okozlovsky@gmail.com>
2020-01-20 22:08:50 +0300

    Fixed training display in HealthRep

--- StressFactor.cs
+++ StressFactor.cs
@@ -12,12 +12,20 @@ namespace KerbalHealth
 
         public override double BaseChangePerDay => HighLogic.CurrentGame.Parameters.CustomParams<KerbalHealthFactorsSettings>().StressFactor;
 
-        double ChangePerDayWithTraining(ProtoCrewMember pcm) => BaseChangePerDay * (1 - Core.KerbalHealthList.Find(pcm).TrainingLevel);
+        /// <summary>
+        /// Returns HP change per day due to stress at the current training level for the kerbal
+        /// </summary>
+        /// <param name="pcm"></param>
+        /// <returns></returns>
+        double ChangePerDayActual(ProtoCrewMember pcm) => BaseChangePerDay * (1 - Core.KerbalHealthList.Find(pcm).TrainingLevel);
 
         public override double ChangePerDay(ProtoCrewMember pcm)
         {
-            if (Core.IsInEditor) return IsEnabledInEditor() ? BaseChangePerDay * (1 - Core.TrainingCap) : 0;
-            return (pcm.rosterStatus == ProtoCrewMember.RosterStatus.Assigned) ? ChangePerDayWithTraining(pcm) : 0;
+            if (Core.IsInEditor)
+                if (IsEnabledInEditor())
+                    return (!Core.TrainingEnabled || KerbalHealthEditorReport.TrainingEnabled) ? BaseChangePerDay * (1 - Core.TrainingCap) : BaseChangePerDay;
+                else return 0;
+            return (pcm.rosterStatus == ProtoCrewMember.RosterStatus.Assigned) ? ChangePerDayActual(pcm) : 0;
         }
     }
 }
