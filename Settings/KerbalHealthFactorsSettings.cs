8b89d3e01304cfab61bac3d65276591b97651eda
GarwelGarwel <okozlovsky@gmail.com>
2020-05-18 10:28:06 +0300

    Removed duplicate factor name localizations

--- Settings/KerbalHealthFactorsSettings.cs
+++ Settings/KerbalHealthFactorsSettings.cs
@@ -43,28 +43,28 @@ namespace KerbalHealth
             }
         }
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Stress", toolTip = "#KH_FS_Stress_desc", minValue = -20, maxValue = 0, displayFormat = "F1", stepCount = 41)] //Stress""HP change per day when the kerbal is assigned; can be lowered through training and/or upgrading Astronaut Complex
+        [GameParameters.CustomFloatParameterUI("#KH_Stress", toolTip = "#KH_FS_Stress_desc", minValue = -20, maxValue = 0, displayFormat = "F1", stepCount = 41)] //Stress""HP change per day when the kerbal is assigned; can be lowered through training and/or upgrading Astronaut Complex
         public float StressFactor = -2;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Confinement", toolTip = "#KH_FS_Confinement_desc", minValue = -10, maxValue = 0, stepCount = 41)]//Confinement""HP change per day in a vessel with 1 living space per kerbal
+        [GameParameters.CustomFloatParameterUI("#KH_Confinement", toolTip = "#KH_FS_Confinement_desc", minValue = -10, maxValue = 0, stepCount = 41)]//Confinement""HP change per day in a vessel with 1 living space per kerbal
         public float ConfinementBaseFactor = -3;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Loneliness", toolTip = "#KH_FS_Loneliness_desc", minValue = -20, maxValue = 0, stepCount = 41)]//Loneliness""HP change per day when the kerbal has no crewmates
+        [GameParameters.CustomFloatParameterUI("#KH_Loneliness", toolTip = "#KH_FS_Loneliness_desc", minValue = -20, maxValue = 0, stepCount = 41)]//Loneliness""HP change per day when the kerbal has no crewmates
         public float LonelinessFactor = -1;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Microgravity", toolTip = "#KH_FS_Microgravity_desc", minValue = -20, maxValue = 0, displayFormat = "F1", stepCount = 41)]//Microgravity""HP change per day when in orbital/suborbital flight or g-force < 0.1
+        [GameParameters.CustomFloatParameterUI("#KH_Microgravity", toolTip = "#KH_FS_Microgravity_desc", minValue = -20, maxValue = 0, displayFormat = "F1", stepCount = 41)]//Microgravity""HP change per day when in orbital/suborbital flight or g-force < 0.1
         public float MicrogravityFactor = -1;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_EVA", toolTip = "#KH_FS_EVA_desc", minValue = -50, maxValue = 0, stepCount = 26)]//EVA""HP change per day when on EVA
+        [GameParameters.CustomFloatParameterUI("#KH_EVA", toolTip = "#KH_FS_EVA_desc", minValue = -50, maxValue = 0, stepCount = 26)]//EVA""HP change per day when on EVA
         public float EVAFactor = -10;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Connected", toolTip = "#KH_FS_Connected_desc", minValue = 0, maxValue = 20, displayFormat = "F1", stepCount = 41)]//Connected""HP change per day when connected to Kerbin
+        [GameParameters.CustomFloatParameterUI("#KH_Connected", toolTip = "#KH_FS_Connected_desc", minValue = 0, maxValue = 20, displayFormat = "F1", stepCount = 41)]//Connected""HP change per day when connected to Kerbin
         public float ConnectedFactor = 0.5f;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_Home", toolTip = "#KH_FS_Home_desc", minValue = 0, maxValue = 20, stepCount = 41)]//Home""HP change per day when in Kerbin atmosphere
+        [GameParameters.CustomFloatParameterUI("#KH_Home", toolTip = "#KH_FS_Home_desc", minValue = 0, maxValue = 20, stepCount = 41)]//Home""HP change per day when in Kerbin atmosphere
         public float HomeFactor = 2;
 
-        [GameParameters.CustomFloatParameterUI("#KH_FS_KSC", toolTip = "#KH_FS_KSC_desc", minValue = 0, maxValue = 20, stepCount = 41)]//At KSC""HP change per day when the kerbal is at KSC (available)
+        [GameParameters.CustomFloatParameterUI("#KH_KSC", toolTip = "#KH_FS_KSC_desc", minValue = 0, maxValue = 20, stepCount = 41)]//At KSC""HP change per day when the kerbal is at KSC (available)
         public float KSCFactor = 4;
 
         [GameParameters.CustomParameterUI("#KH_FS_TrainingEnabled", toolTip = "#KH_FS_TrainingEnabled_desc")]//Training Enabled""Turn on/off the need to train kerbals to reduce stress
