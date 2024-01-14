47fe25e6f56342a64c2945a1fc47bd4e3188c017
Garwel <garwel@protonmail.com>
2024-01-14 15:54:35 +0100

    Update to v1.6.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KerbalHealth")]
-[assembly: AssemblyCopyright("Copyright © Garwel 2017-2023")]
+[assembly: AssemblyCopyright("Copyright © Garwel 2017-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.5")]
-[assembly: AssemblyFileVersion("1.6.5")]
+[assembly: AssemblyVersion("1.6.6")]
+[assembly: AssemblyFileVersion("1.6.6")]
 [assembly: NeutralResourcesLanguage("en")]
